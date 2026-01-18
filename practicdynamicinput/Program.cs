82f01e56055e9def3115fc4db736f6f022140664
muzamil537 <muzamilshakeel537@gmail.com>
2026-01-18 15:00:41 +0530

    kar aaa

--- practicdynamicinput/Program.cs
+++ practicdynamicinput/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace practicdynamicinput;
-
+﻿using System;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("enter your number ");
+         int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("enter your number ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+        int result = num + num1;
+
+        Console.WriteLine("The sume of 2 numbers :" + result);
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("enter your name");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("enter your age");
+//int age = Convert.ToInt32(Console.Read());
+
+//Console.WriteLine(" my name is " + name + " and \n i am " + age + " years old");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("enter your name");
+//string name= Console.ReadLine();
+
+//Console.WriteLine("enter your age :");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("my name is " + name);
+//Console.WriteLine("my age is " + age);
